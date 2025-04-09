56604fc9062f0cfb126221ec6ae8524491809923
ravinithishkumar2001 <ravinithishkumar2001@gmail.com>
2025-04-10 00:13:04 +0530

    Update AllRoutes.jsx

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -5,7 +5,7 @@ const AllRoutes = () => {
   return (
     <>
     <Routes>
-        <Route path="/" element={<MovieList title="Your Guide to Great Movies" apiPath="movie/now_playing"/>}/>
+        <Route path="/Fiction" element={<MovieList title="Your Guide to Great Movies" apiPath="movie/now_playing"/>}/>
         <Route path="/movies/popular" element={<MovieList title="Popular Movies" apiPath="movie/popular"/>}/>
         <Route path="/movies/top" element={<MovieList title="Top Rated Movies" apiPath="movie/top_rated"/>}/>
         <Route path="/movies/upcoming" element={<MovieList title="Upcoming Movies" apiPath="movie/upcoming"/>}/>
